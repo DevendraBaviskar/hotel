fdaf8ac2302a7c5ddb4c2bf4629fb401e6c680cc
Devendra <devendrabaviskar22@gmail.com>
2024-09-20 17:57:38 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Building from "./Images/Building.jpg";
+import Lobby from "./Images/Lobby.jpg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <img src={Building} alt="Building" />
+      <img src={Lobby} alt="Lobby" />
     </div>
   );
 }
